1bbd7c15a1e5a179bf38c384f8b5d97fb56527cc
Chris <lefatoum@hotmail.fr>
2020-09-17 16:08:51 +0200

    Final2

--- config.php
+++ config.php
@@ -9,6 +9,7 @@ define('DB_NAME', 'test9');
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
